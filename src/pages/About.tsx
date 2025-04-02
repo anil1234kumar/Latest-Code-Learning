261663b5c8bd8acfb89fe2c8a43a13d29749ddcd
anil <anil.kumar@antiersolutions.com>
2025-04-02 16:11:03 +0530

    add scss in boler plate

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,15 +1,12 @@
-import { Typography } from 'antd';
-import { useTitle } from '@/hooks/useTitle';
+import { Button } from 'antd';
+import './About.scss';
 
-const { Title, Paragraph } = Typography;
-
-export function About() {
-  useTitle('About');
-
+export const About: React.FC = () => {
   return (
-    <Typography>
-      <Title level={2}>About</Title>
-      <Paragraph>Learn more about our project and team.</Paragraph>
-    </Typography>
+    <div>
+      <Button className="checkScss" onClick={() => console.log('clicked')}>
+        Click me
+      </Button>
+    </div>
   );
-}
+};
